6c09f16d453f61d93fe6afd732edf569adf221f9
VictorM114 <VictorMAyala11@gmail.com>
2021-01-27 21:09:20 -0400

    First Commit
    This is the first commit of the Udemy Course that I am doing.

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+/*1. Declare variables called 'country', 'continent' and 'population' and
+     assign their values according to your own country (population in millions)
+  2. Log their values to the console */
+
+let country = "Puerto Rico";
+let continent = "Caribbean";
+let population = 3000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
