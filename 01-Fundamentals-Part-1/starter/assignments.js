ce1d4f9d0f2952ef2600260ff52eccad727e3826
Víctor Ayala <42361783+VictorM114@users.noreply.github.com>
2021-07-05 21:10:08 -0400

    First Lecture Done
    I did the first lecture that talked about Variables.

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+/*First Assignment Lecture: Values and Variables*/
+let country = "Puerto Rico";
+let continent = "Caribbean";
+let population = "3 million";
+console.log(country);
+console.log(continent);
+console.log(population);
