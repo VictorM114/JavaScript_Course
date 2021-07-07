7ef0bee614e6658c4685d3ee58c65612bc9ed8df
Víctor Ayala <42361783+VictorM114@users.noreply.github.com>
2021-07-06 22:04:37 -0400

    Data Types
    Hoy lei la sección de Data Types en el curso.

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,8 @@
-/*First Assignment Lecture: Values and Variables*/
+/* First Assignment Lecture: Values and Variables
+Declarar 3 variables de país continente y población
 let country = "Puerto Rico";
 let continent = "Caribbean";
 let population = "3 million";
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(population); */
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'Amazing';
+/* let js = 'Amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -8,5 +8,43 @@ let firstName = "Matilda";
 
 console.log(firstName);
 console.log(firstName);
-console.log(firstName);
+console.log(firstName); 
 
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+// Esto typeof es como un comando/parametro que te dice que tipo
+// de datos tiene esa variable en el console log
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Victor');
+
+// Para cambiarle el valor a una variable no debemos escribir let nuevamente
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // esto es un bug en JS
+*/
+
+let age = 30;
+age = 31;
+
+// Const se utiliza para variables constantes que no se pueden cambiar
+const birthYear = 1991;
+
+// Las variables constantes no se pueden declarar vacias
+// const job;
+
+// Para mejor practicas se debe utilizar const siempre a menos que estemos
+// seguros que el valor de la variable debe cambiar
+
+// Var es otra forma de declarar variables que ya no se utiliza
+var job = "programmer";
+job = 'teacher';
